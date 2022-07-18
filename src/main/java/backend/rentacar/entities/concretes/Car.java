df7329c9bf292134291f2f8608ab39422ec1bacd
Görkem <gorkem_yelken@hotmail.com>
2022-07-18 11:57:38 +0300

    Car nesnesi oluşturuldu.

--- src/main/java/backend/rentacar/entities/concretes/Car.java
+++ src/main/java/backend/rentacar/entities/concretes/Car.java
@@ -2,4 +2,18 @@ package backend.rentacar.entities.concretes;
 
 public class Car {
 
+    private int carId;
+
+    private Brand brand;
+
+    private Color color;
+
+    private String carName;
+
+    private double dailyPrice;
+
+    private int modelYear;
+
+
+    private String description;
 }
