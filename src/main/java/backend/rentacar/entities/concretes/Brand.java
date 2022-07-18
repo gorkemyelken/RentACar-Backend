b25c1227450ca37fa6b1134f60a33432612f2bfc
Görkem <gorkem_yelken@hotmail.com>
2022-07-18 11:59:20 +0300

    Brand nesnesi oluşturuldu.

---
+++ src/main/java/backend/rentacar/entities/concretes/Brand.java
@@ -0,0 +1,8 @@
+package backend.rentacar.entities.concretes;
+
+public class Brand {
+
+    private int brandId;
+
+    private String brandName;
+}
--- src/main/java/backend/rentacar/entities/concretes/Car.java
+++ src/main/java/backend/rentacar/entities/concretes/Car.java
@@ -14,6 +14,5 @@ public class Car {
 
     private int modelYear;
 
-
     private String description;
 }
