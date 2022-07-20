36ef8fe42aaf259062b4e23817bdde1a1742b016
Görkem <gorkem_yelken@hotmail.com>
2022-07-20 11:35:47 +0300

    Result yapılandırması getirildi.

---
+++ src/main/java/backend/rentacar/core/utilities/results/ErrorResult.java
@@ -0,0 +1,12 @@
+package backend.rentacar.core.utilities.results;
+
+public class ErrorResult extends Result{
+
+    public ErrorResult() {
+        super(false);
+    }
+
+    public ErrorResult(String message) {
+        super(false, message);
+    }
+}
