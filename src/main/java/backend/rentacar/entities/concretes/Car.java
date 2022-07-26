1d237b8dad0566b66713bc3607333ceabb8677ad
Görkem <gorkem_yelken@hotmail.com>
2022-07-26 09:05:24 +0300

    @JsonIgnoreProperties eklendi.

--- src/main/java/backend/rentacar/entities/concretes/Car.java
+++ src/main/java/backend/rentacar/entities/concretes/Car.java
@@ -1,5 +1,6 @@
 package backend.rentacar.entities.concretes;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import java.util.List;
 @Table(name="cars")
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","rental"})
 public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/backend/rentacar/entities/concretes/Customer.java
+++ src/main/java/backend/rentacar/entities/concretes/Customer.java
@@ -1,5 +1,6 @@
 package backend.rentacar.entities.concretes;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @Entity
 @Table(name = "customers")
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","rentals"})
 public class Customer{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
