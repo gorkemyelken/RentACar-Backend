becc3cf2d2ea3d6618c5f3405d91491b14894dd7
Görkem <gorkem_yelken@hotmail.com>
2022-08-04 13:20:12 +0300

    Global messages eklendi.

---
+++ src/main/java/backend/rentacar/business/constants/Messages.java
@@ -0,0 +1,11 @@
+package backend.rentacar.business.constants;
+
+public class Messages {
+
+    public class GlobalMessages{
+        public static final String DATA_LISTED_SUCCESSFULLY = "Data listed successfully.";
+        public static final String DATA_ADDED_SUCCESSFULLY = "Data added successfully.";
+        public static final String DATA_UPDATED_SUCCESSFULLY = "Data updated successfully.";
+        public static final String DATA_DELETED_SUCCESSFULLY = "Data deleted successfully.";
+    }
+}
