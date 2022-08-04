aacec7c7e11a445f4afd45afdf29ff09301e332b
Görkem <gorkem_yelken@hotmail.com>
2022-08-04 17:02:13 +0300

    CarImagesController update ve delete eklendi.

--- src/main/java/backend/rentacar/controllers/CarImagesController.java
+++ src/main/java/backend/rentacar/controllers/CarImagesController.java
@@ -2,14 +2,13 @@ package backend.rentacar.controllers;
 
 import backend.rentacar.business.abstracts.CarImageService;
 import backend.rentacar.entities.dtos.carimagedto.CarImageCreateDto;
+import backend.rentacar.entities.dtos.carimagedto.CarImageUpdateDto;
+import backend.rentacar.entities.dtos.userdto.UserUpdateDto;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -32,5 +31,14 @@ public class CarImagesController {
     @PostMapping("/add")
     public ResponseEntity<?> add(@Valid @RequestBody CarImageCreateDto carImageCreateDto){
         return new ResponseEntity(this.carImageService.add(carImageCreateDto), HttpStatus.CREATED);
+    }
+    @PutMapping("/update")
+    public ResponseEntity<?> update(@RequestParam int carImageId, @Valid @RequestBody CarImageUpdateDto carImageUpdateDto){
+        return new ResponseEntity<>(this.carImageService.update(carImageId, carImageUpdateDto), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> delete(@RequestParam int carImageId){
+        return new ResponseEntity<>(this.carImageService.delete(carImageId), HttpStatus.OK);
     }
 }
