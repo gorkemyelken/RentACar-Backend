46a2886159f7d3da289c002f755989d8722e6cf3
Görkem <gorkem_yelken@hotmail.com>
2022-08-04 13:55:30 +0300

    User messages eklendi.

--- src/main/java/backend/rentacar/business/concretes/UserManager.java
+++ src/main/java/backend/rentacar/business/concretes/UserManager.java
@@ -1,6 +1,7 @@
 package backend.rentacar.business.concretes;
 
 import backend.rentacar.business.abstracts.UserService;
+import backend.rentacar.business.constants.Messages;
 import backend.rentacar.core.utilities.mapping.ModelMapperService;
 import backend.rentacar.core.utilities.results.DataResult;
 import backend.rentacar.core.utilities.results.SuccessDataResult;
@@ -30,19 +31,19 @@ public class UserManager implements UserService {
     public DataResult<List<UserViewDto>> getAll() {
         List<User> users = this.userRepository.findAll();
         List<UserViewDto> result = users.stream().map(user -> this.modelMapperService.forDto().map(user, UserViewDto.class)).collect(Collectors.toList());
-        return new SuccessDataResult<>(result);
+        return new SuccessDataResult<>(result, Messages.GlobalMessages.DATA_LISTED);
     }
 
     @Override
     public DataResult<UserViewDto> add(UserCreateDto userCreateDTO) {
         User user = this.userRepository.save(new User(userCreateDTO.getEmail(),userCreateDTO.getPassword()));
-        return new SuccessDataResult<>(UserViewDto.of(user));
+        return new SuccessDataResult<>(UserViewDto.of(user), Messages.GlobalMessages.DATA_ADDED);
     }
 
     @Override
     public DataResult<UserViewDto> findByEmail(String email) {
         User user = this.userRepository.findByEmail(email);
         UserViewDto result = this.modelMapperService.forDto().map(user, UserViewDto.class);
-        return new SuccessDataResult<>(result);
+        return new SuccessDataResult<>(result, Messages.UserMessages.USER_LISTED_BY_EMAIL);
     }
 }
