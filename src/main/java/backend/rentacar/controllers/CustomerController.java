82802de8aa46c845bcde5fdd01ad77304271f026
Görkem <gorkem_yelken@hotmail.com>
2022-08-04 17:04:54 +0300

    CustomersController update ve delete eklendi.

--- src/main/java/backend/rentacar/controllers/CustomerController.java
+++ src/main/java/backend/rentacar/controllers/CustomerController.java
@@ -2,14 +2,13 @@ package backend.rentacar.controllers;
 
 import backend.rentacar.business.abstracts.CustomerService;
 import backend.rentacar.entities.dtos.customerdto.CustomerCreateDto;
+import backend.rentacar.entities.dtos.customerdto.CustomerUpdateDto;
+import backend.rentacar.entities.dtos.userdto.UserUpdateDto;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -31,5 +30,14 @@ public class CustomerController {
     public ResponseEntity add(@Valid @RequestBody CustomerCreateDto customerCreateDto){
 
         return new ResponseEntity(this.customerService.add(customerCreateDto), HttpStatus.CREATED);
+    }
+    @PutMapping("/update")
+    public ResponseEntity<?> update(@RequestParam int customerId, @Valid @RequestBody CustomerUpdateDto customerUpdateDto){
+        return new ResponseEntity<>(this.customerService.update(customerId, customerUpdateDto), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> delete(@RequestParam int customerId){
+        return new ResponseEntity<>(this.customerService.delete(customerId), HttpStatus.OK);
     }
 }
